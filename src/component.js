20ffbaf6bca2f6277b02abee6d9fb12a872cf0b4
Ryan Mahoney <ryan.mahoney@betterlesson.com>
2016-11-22 17:47:07 -0500

    work in progress

---
+++ src/component.js
@@ -0,0 +1,107 @@
+
+// peer dependencies
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import reqwest from 'reqwest';
+import nprogress from 'nprogress';
+
+// local dependencies
+import helper from './helper.js';
+
+// expects all Actions, Routes and a 404 React component to be passed in
+export default function (Routes, Actions, Unknown) {
+
+    function mapDispatchToProps(dispatch) {
+        return bindActionCreators(Actions, dispatch);
+    }
+
+    const mapStateToProps = function (state) {
+        return state;
+    };
+
+    return connect(mapStateToProps, mapDispatchToProps)(React.createClass({
+
+        handleUnlisten: function () {},
+
+        componentDidMount: function () {
+            // listen for changes to the current location
+            this.handleUnlisten = appHistory.listen(function (location, action) {
+
+                // decide which path to call
+                var path;
+                const guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                    return v.toString(16);
+                });
+                if (location.pathname.indexOf('?') !== -1) {
+                    path = location.pathname + '&guid=' + guid;
+                } else {
+                    path = location.pathname + '?guid=' + guid;
+                }
+
+                // clear and start
+                nprogress.done();
+                nprogress.start();
+
+                // do XHR request
+                reqwest({
+                    method: 'get',
+                    url: path,
+                    type: 'json',
+                    contentType: 'application/json',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                }).then(function (response) {
+
+                    // update store
+                    response.location = location.pathname;
+                    this.props.changeHistory(response);
+                    nprogress.done();
+
+                }.bind(this), function (err, msg) {
+                    console.log('XHR ERROR');
+                    console.log(err);
+                    this.props.changeHistory({location: location.pathname});
+                    nprogress.done();
+                }.bind(this));
+
+            }.bind(this));
+        },
+
+        componentWillUnmount: function () {
+            this.handleUnlisten();
+        },
+
+        render: function () {
+
+            // get the component from the router
+            var route = RouteHelper.match(Routes, this.props.location, Unknown);
+            var Component = route.component;
+
+            // we may not send all the props, depending if there is a reducerKey
+            var props = {};
+            if (route.reducerKey) {
+                // include all the action functions
+                Object.keys(this.props).map(function (propKey) {
+                    // if it is a function, must be a redux action function
+                    if (Object.prototype.toString.call(this.props[propKey]) === '[object Function]') {
+                        props[propKey] = this.props[propKey];
+                    }
+                }.bind(this));
+
+                // put the reducerKey properties at the top of the props, ignore other keys
+                Object.keys(this.props[route.reducerKey]).map(function (prop) {
+                    props[prop] = this.props[route.reducerKey][prop];
+                }.bind(this));
+            } else {
+                // send all the props
+                props = Object.assign(props, this.props, {});
+            }
+
+            // return the component from the router with the appropriate props
+            return (<Component {...props} />);
+        }
+    }));
+}
---
+++ src/helper.js
@@ -0,0 +1,64 @@
+// peer dependencies
+import React from 'react';
+
+// regular dependency
+import pathToRegexp from 'path-to-regexp';
+
+export default {
+    match: function (Routes, location, Unknown) {
+        // loop through all the routes
+        const route = Routes.map(function(route) {
+            // attempt to match
+            if (route.re.exec(location)) {
+                return route;
+            }
+
+        // remove null elements
+        }).filter(function (route) {
+            if (route) {
+                return true;
+            }
+            return false;
+
+        // condense array to object
+        }).reduce(function (result, item) {
+            return item;
+        }, {});
+
+        // we couldn't match anything
+        if (Object.keys(route).length == 0) {
+            return {
+                component: Unknown
+            };
+        }
+
+        // send the route
+        return route;
+    },
+
+    prepare: function (Routes) {
+
+        // loop over all routes
+        return Object.keys(Routes).map(function (route) {
+            var routeKeys = [];
+            var re = pathToRegexp(route, routeKeys);
+            var component, reducer;
+
+            // handle a case where we want to be able to filter props by reducer key later
+            if (Object.prototype.toString.call(Routes[route]) === '[object Array]') {
+                component = Routes[route][0];
+                reducer = Routes[route][1];
+            } else {
+                component = Routes[route];
+            }
+
+            // send out a more useful data structure
+            return {
+                re: re,
+                keys: routeKeys,
+                component: component,
+                reducerKey: reducer
+            };
+        });
+    }
+};
