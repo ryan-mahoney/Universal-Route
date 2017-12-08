5fa438fb7f5b10326835a4442589a2b9d6c3c357
Ryan Mahoney <ryan@virtuecenter.com>
2017-12-07 21:46:00 -0500

    experimnet

--- src/action.js
+++ src/action.js
@@ -1,19 +1,6 @@
-
 export const CHANGE_PAGE = 'CHANGE_PAGE';
-export const CHANGE_PAGE_ERROR = 'CHANGE_PAGE_ERROR';
-export const CHANGE_PAGE_AUTH = 'CHANGE_PAGE_AUTH';
 
 export const changePage = (data) => ({
   type: CHANGE_PAGE,
-  data: data.payload
-});
-
-export const changePageError = (error) => ({
-  type: CHANGE_PAGE_ERROR,
-  error: error
-});
-
-export const changePageAuth = (auth) => ({
-  type: CHANGE_PAGE_AUTH,
-  auth: auth
+  data: data
 });
