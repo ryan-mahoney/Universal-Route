f9721ebbe4291985ef7851cee507b7db9e2b00b5
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-24 12:36:18 -0400

    remove cruft

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["module", "./router", "./helper", "./reducer", "./action"], factory);
+    define(["module", "./router", "./helper"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(module, require("./router"), require("./helper"), require("./reducer"), require("./action"));
+    factory(module, require("./router"), require("./helper"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod, global.router, global.helper, global.reducer, global.action);
+    factory(mod, global.router, global.helper);
     global.index = mod.exports;
   }
-})(this, function (module, _router, _helper, _reducer, _action) {
+})(this, function (module, _router, _helper) {
   "use strict";
 
   var _helper2 = _interopRequireDefault(_helper);
@@ -25,11 +25,6 @@
     Link: _router.Link,
     navigate: _router.navigate,
     createRouter: _router.createRouter,
-    routesHelper: _helper2.default,
-    pageReducer: _reducer.reducer,
-    routerActions: {
-      CHANGE_PAGE: _action.CHANGE_PAGE,
-      changePage: _action.changePage
-    }
+    routesHelper: _helper2.default
   };
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import { Link, navigate, createRouter } from "./router";
 import helper from "./helper";
-import { reducer } from "./reducer";
-import { CHANGE_PAGE, changePage } from "./action";
 
 module.exports = {
   Link: Link,
   navigate: navigate,
   createRouter: createRouter,
-  routesHelper: helper,
-  pageReducer: reducer,
-  routerActions: {
-    CHANGE_PAGE,
-    changePage
-  }
+  routesHelper: helper
 };
