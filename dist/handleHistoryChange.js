0555a9e58e99a490cfa5c70cfda2a702ca889107
Ryan Mahoney <ryan@virtuecenter.com>
2021-03-01 11:17:31 -0500

    expose app history

--- dist/handleHistoryChange.js
+++ dist/handleHistoryChange.js
@@ -27,7 +27,7 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-// initilize a place-holder for the last request cancellation token
+// initialize a place-holder for the last request cancellation token
 var requestCancellation = false;
 var lastLocation = null;
 
--- src/history.js
+++ src/history.js
@@ -1,11 +1,7 @@
 import { createBrowserHistory } from "history";
 
 // create app history if possible, as singleton
-const appHistory =
-  typeof window !== "undefined" &&
-  window.document &&
-  window.document.createElement
-    ? createBrowserHistory()
-    : false;
+export const appHistory =
+  typeof window !== "undefined" && window.document && window.document.createElement ? createBrowserHistory() : false;
 
 export default appHistory;
